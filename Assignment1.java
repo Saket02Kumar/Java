92d3811f4efe036db23718faaf9ccf95f0eabadd
Saket02Kumar <122230666+Saket02Kumar@users.noreply.github.com>
2023-01-11 01:10:13 +0530

    Uploading

---
+++ Assignment1.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+
+
+public class Assignment1 {
+	
+
+	public static void main(String[] args) {
+		char operation, con;
+		Double num1,num2,answer;
+		
+		Scanner scanner= new Scanner(System.in);
+		
+		do {
+			System.out.print("Enter First Number: ");
+			num1=scanner.nextDouble();
+			
+			System.out.print("Enter Second Number: ");
+			num2=scanner.nextDouble();
+			System.out.println("============================");
+			System.out.print("Choose an operation: ");
+			operation=scanner.next().charAt(0);
+		switch(operation) {
+		
+		case '+':
+			
+			answer=num1+num2;
+			
+			System.out.println("Addition of Number = "+answer);
+			break;
+		
+		case '-':
+			answer=Math.abs(num1-num2);
+			System.out.println("Subtraction of Number = "+answer);
+			break;
+			
+		case '*':
+			answer=num1*num2;
+			System.out.println("Multiplication of Number = "+answer);
+			break;
+		case '/':
+			answer=num1/num2;
+			System.out.println("Division of Number = "+answer);
+			break;
+		default:
+			System.out.println("Invalid Operation selected");
+			
+			}
+		System.out.println("===============================");
+		System.out.print("Do you want to continue: ");
+			con=scanner.next().charAt(0);
+			
+			System.out.println("==============================");
+		}
+		while(con=='y'||con=='y');
+		
+
+	}
+
+}
---
+++ arrays.java
@@ -0,0 +1,17 @@
+public class arrays {
+     public static void main(String[] args) {
+         int[] marks=new int[3];
+         marks[0]=67;
+         marks[1]=68;
+         marks[2]=89;
+         //System.out.println(marks[0]);
+         //System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+        
+        
+        
+     }
+}
